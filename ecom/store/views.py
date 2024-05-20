09bdf203252b3abb13e525eff108e17cf0749a67
pratikshya <phoebbbey@gmail.com>
2024-05-21 00:37:35 +0545

    changes to be committed

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from .models import Product
+from django.contrib.auth.forms import UserCreationForm  
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  # Use your custom form
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the home page (or another view)
+    else:
+        form = UserCreationForm()  # Create an empty form
+
+    context = {'form': form}
+    return render(request, 'store/register.html', context)
 
 def home(request):
-    return render(request ,"app/index.html")
+    products=Product.objects.all()
+    return render(request ,"store/index.html",{'products':products})
+
