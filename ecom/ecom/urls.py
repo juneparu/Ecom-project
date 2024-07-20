2e754ff6d79c4c662cfc277f309881fedc5aac7b
unknown <rubinarai31@gmail.com>
2024-07-20 15:47:46 +0545

    merged and login

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -1,22 +1,20 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from django.urls import path ,include
 from . import settings
-=======
+
 from django.urls import path ,include 
 from django.conf import settings
->>>>>>> f5d05fd35d7a467215f6d2e0008bbd1bf1f7dbd2
+
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-<<<<<<< HEAD
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-    
-]+static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
->>>>>>> f5d05fd35d7a467215f6d2e0008bbd1bf1f7dbd2
+
+
 
